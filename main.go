6b9f83c124fa8770d58dd023fa2016423d2133cb
sujay2306 <sujayks007@gmail.com>
2023-08-18 19:31:18 +0530

    added servers as slices

--- main.go
+++ main.go
@@ -1,18 +1,28 @@
 package main
 
 import (
-	
 	"log"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
 )
 
+var (
+	serverList = []string{
+			"http://127.0.0.1:5000",
+	 		"http://127.0.0.1:5001",
+			"http://127.0.0.1:5000",
+			"http://127.0.0.1:5003",
+			"http://127.0.0.1:5004",
+
+		}
+	lastServedIndex = 0
+
+)
 func main(){
 	http.HandleFunc("/", forwardRequest)
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
-
 //Todo:
 // We need to forward the request to flask server
 //we need to create a single host reverse proxy using http util
@@ -20,10 +30,10 @@ func forwardRequest(res http.ResponseWriter ,req *http.Request) {
 	url := getServer()
 	rProxy := httputil.NewSingleHostReverseProxy(url) 	 //this accepts a url
 	rProxy.ServeHTTP(res, req) 			//takes reswriter and req as i/p
- 
 }
 
 func getServer() *url.URL{
-	url, _ := url.Parse("http://127.0.0.1:5000/")
+	url, _ := url.Parse(serverList[(lastServedIndex+1)%5]) //make sure range is always 5
+	lastServedIndex ++ 
 	return url
 }
